13818396867345200ba92ca3b1740dd61740a2f8
astraknots@gmail.com <genskrz@Genevieves-Laptop.local>
2023-01-16 21:20:55 -0700

    Added some more patterns and upped my test

--- tests/test_chartWriter.py
+++ tests/test_chartWriter.py
@@ -6,7 +6,7 @@ import chartWriter as cw
 class TestChartWriter(TestCase):
     def test_inc_cnt_to_next_sign(self):
         # Test that degree increments will increment to the next sign after 30 degrees
-        for deginc in range(1, 13):
+        for deginc in range(1, 30):
             cnt_true = 0
             with self.subTest(deginc=deginc):
                 for x_d in range(0, 360, deginc): # loop over all 360 degrees
