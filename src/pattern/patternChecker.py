8476ba0acd426626526d75eb4747c50ddc8d943a
astraknots <astraknots@gmail.com>
2023-05-20 20:55:46 -0600

    Fixed the main parse method to get the last instruction even if no separator. Added a json file parser to read in special sts and get their st count. Still not handling nested repeats

--- src/pattern/patternChecker.py
+++ src/pattern/patternChecker.py
@@ -2,12 +2,13 @@ import getopt
 import logging
 import sys
 import re
+import json
 
 SEPARATORS = [',', '.']
-REPEAT_START = ['(', '[']
-REPEAT_END = [')', ']']
+REPEAT_START = ['(', '[', '*']
+REPEAT_END = [')', ']', '*']
 REPEAT_WORDS = {'ONCE': 1, 'TWICE': 2}
-OPERATION_CNT = {'K':1, 'P':1, 'SM':0, 'YO':1, '2TOG':1 }
+OPERATION_CNT = {'K': 1, 'P': 1, 'SM': 0, 'YO': 1, '2TOG': 1}
 
 
 def get_rep_end_for_start(rep_start_str):
@@ -35,34 +36,46 @@ def parse_patt_str(patt_str, skip_repeat=False):
     instrs = []
     single = ''
 
+    # Loop over each char in the instr str
     for idx in range(0, len(patt_str)):
         char = patt_str[idx]
+
+        # if we have the beginning of a repeat character
         if not skip_repeat and char in REPEAT_START:
-            rem_patt_str = patt_str[idx+1:]
+            # get the rest of the str after that char
+            rem_patt_str = patt_str[idx + 1:]
+            # find the index of the end of repeat char in remaining str
             rep_end_idx = rem_patt_str.find(get_rep_end_for_start(char), 1)
+            # parse the instructions for repeat - makes a recursive call that will skip this section, thus skip_repeat = True
             rep_instrs = parse_patt_str(rem_patt_str[0:rep_end_idx], True)
+            # get index of the end of repeat
             end_rep_instru_idx = rem_patt_str.find(SEPARATORS[0], rep_end_idx)
-            rep_reps = determine_rep_reps(rem_patt_str[rep_end_idx+1:end_rep_instru_idx])
+            # determine how many times this should repeat
+            rep_reps = determine_rep_reps(rem_patt_str[rep_end_idx + 1:end_rep_instru_idx])
+            # Add the instrs repeated out as individual instructions
             added = 0
-            while(added < rep_reps):
+            while (added < rep_reps):
                 instrs.extend(rep_instrs)
                 added += 1
 
             # recursively call this on the rem_patt_str after this repeat, and extend the result
-            instrs.extend(parse_patt_str(rem_patt_str[end_rep_instru_idx+1:]))
+            instrs.extend(parse_patt_str(rem_patt_str[end_rep_instru_idx + 1:]))
             break
 
         elif char in SEPARATORS:
+            # when we hit a separator (i.e. ,) we've gathered a whole instruction, add to array
             instrs.append(single.strip())
             single = ''
             continue
         else:
+            # gather another character of an instruction
             single += char
 
-    if skip_repeat and len(single) > 0:
+    # This captures the last fully gathered instruction, once we've run out of separators that indicate the end
+    if len(single.strip()) > 0:
         instrs.append(single.strip())
 
-    print(instrs)
+    print(instrs)  # debug
     return instrs
 
 
@@ -70,9 +83,9 @@ def get_num_from_str(a_str):
     # get the numbers of the instr
     match_str = '[0-9]$'
     a_cnt_search = re.search(match_str, a_str)
-    #print("A patt has cnt:", a_cnt_search)
+    # print("A patt has cnt:", a_cnt_search)
     a_cnt_list = re.findall(match_str, a_str)
-    #print("A cnt:", a_cnt_list)
+    # print("A cnt:", a_cnt_list)
     if a_cnt_list and len(a_cnt_list) > 0:
         st_cnt = 0
         for a_cnt in a_cnt_list:
@@ -96,12 +109,25 @@ def count_instr_sts(instr):
     return cnt
 
 
-def count_sts(patt_instr_list):
+def count_sts(patt_instr_list, specials=[]):
+    '''Count the number of sts this list of instrs involves - gives the count AFTER executing the sts'''
     total_cnt = 0
+
     for patt_instr in patt_instr_list:
-        total_cnt += count_instr_sts(patt_instr.upper())
+        match = False
+        if len(specials) > 0:
+            for special in specials:
+                if patt_instr == special["stitch-name"]:
+                    cnt = special["stitch-count"]
+                    total_cnt += cnt
+                    print(f"{patt_instr} has {cnt} st{'s' if cnt > 1 else ''}")
+                    match = True
+
+        if not match:
+            total_cnt += count_instr_sts(patt_instr.upper())
 
     return total_cnt
+
 
 '''    
     st_cnt = 0
@@ -138,6 +164,35 @@ def count_sts(patt_instr_list):
 '''
 
 
+def parse_special_patt_instrs(filename):
+    '''Expected format for special pattern instru:
+    { "stitch-name": <str stitch-name> i.e. "LC",
+      "instr": <str normal separated list of knit instructions> i.e. "sl 1 st to cable needle and hold in front, k2, k1 from cable needle",
+      "stitch-count": <int number of sts included in this special stitch> i.e. 3
+      (optional) "separator": <str separator> i.e. ','
+    }
+    TOdo: For patterns? inc row instrs -- see/leverage patternUtil
+    '''
+    specials = [] # list of dictionaries of json read in
+    # Read in the file
+    raw_data = ""
+    with open(filename) as f:
+        for line in f:
+            raw_data += line
+            if '}' in line: # then we've reached the end of this, parse and store
+                # parse from json format into python dict
+                data = json.loads(raw_data)
+                specials.append(data)
+                raw_data = ""
+
+    for item in specials:
+        print(item)
+        #print(item, "=", data[item])
+
+    # print(json.dumps(data))
+    return specials
+
+
 def check_pattern(argv):
     filename = ''
     patt_str = ''
@@ -160,14 +215,15 @@ def check_pattern(argv):
     if filename == '':
         logging.info("No filename of pattern given")
     else:
-        logging.info("Filename of pattern given:" + filename
-                     )
+        logging.info("Filename of pattern given:" + filename)
+        specials = parse_special_patt_instrs(filename)
 
     if patt_str == '':
         logging.info("No pattern string given")
+        return
 
     parsed_instr = parse_patt_str(patt_str)
-    st_cnt = count_sts(parsed_instr)
+    st_cnt = count_sts(parsed_instr, specials)
     print("Found a total stitch count of:", st_cnt)
 
 
