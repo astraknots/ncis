765237412926239214bfaa9a5b0ddd8c200119fb
astraknots <astraknots@gmail.com>
2024-10-16 15:35:46 -0600

    adding some experimental files

--- datas/gatherData.py
+++ datas/gatherData.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import PyPDF2, re
+
+#import lxml, html5lib, beautifulsoup4
 
 #dfAstraFall = pandas.read_csv('./Astrology Knitting Dignity - Fall.csv')
 
@@ -6,12 +10,15 @@ import pandas as pd
 
 knitDigFile = pd.ExcelFile('./Astrology Knitting Dignity.xlsx')
 kdf_sheet_names = knitDigFile.sheet_names
-print(kdf_sheet_names)
+#print(kdf_sheet_names)
 
 dfAstra = {}
 for sheet_name in kdf_sheet_names:
     dfa_sheet = pd.read_excel('./Astrology Knitting Dignity.xlsx', sheet_name)
     dfAstra[sheet_name] = dfa_sheet
 
-print(dfAstra)
+#print(dfAstra)
+
+gkAstraHtmlFile = pd.read_html('./GKNatal Chart Report.html')
+print(gkAstraHtmlFile)
 
