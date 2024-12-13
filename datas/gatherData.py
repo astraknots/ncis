a1269966014af5f9e855fa7601665da939e32ed4
astraknots <astraknots@gmail.com>
2024-12-13 13:17:47 -0700

    CLeaned up gathering data and cleaning dates from reading an excel

--- datas/gatherData.py
+++ datas/gatherData.py
@@ -2,23 +2,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import PyPDF2, re
 
-#import lxml, html5lib, beautifulsoup4
 
-#dfAstraFall = pandas.read_csv('./Astrology Knitting Dignity - Fall.csv')
+def read_excel_file_into_sheet_dict(_filename):
+    sheet_dict = {}
+    xfile = pd.ExcelFile(_filename)
+    xsheet_names = xfile.sheet_names
+    for sheet_name in xsheet_names:
+        df_sheet = pd.read_excel(_filename, sheet_name)
+        sheet_dict[sheet_name] = df_sheet
+    return sheet_dict
 
-#print(dfAstraFall)
 
-knitDigFile = pd.ExcelFile('./Astrology Knitting Dignity.xlsx')
-kdf_sheet_names = knitDigFile.sheet_names
-#print(kdf_sheet_names)
+def clean_dates_in_df(_df, _df_date_col_name, _printc=False):
+    # Clean the dates into dates
+    _df[_df_date_col_name] = pd.to_datetime(_df[_df_date_col_name])
+    if _printc:
+        print(_df.to_string())
 
-dfAstra = {}
-for sheet_name in kdf_sheet_names:
-    dfa_sheet = pd.read_excel('./Astrology Knitting Dignity.xlsx', sheet_name)
-    dfAstra[sheet_name] = dfa_sheet
 
-#print(dfAstra)
-
-gkAstraHtmlFile = pd.read_html('./GKNatal Chart Report.html')
-print(gkAstraHtmlFile)
+dfs_dict = read_excel_file_into_sheet_dict('./G Knitting Projects 2004-2024.xlsx')
+for a_sheet in dfs_dict:
+    if a_sheet == 'Projects':
+        clean_dates_in_df(dfs_dict[a_sheet], 'Started')
+        clean_dates_in_df(dfs_dict[a_sheet], 'Completed', True)
+    elif a_sheet == 'Mercury Retrogrades':
+        clean_dates_in_df(dfs_dict[a_sheet], 'Retrograde Start')
+        clean_dates_in_df(dfs_dict[a_sheet], 'Retrograde End', True)
 
